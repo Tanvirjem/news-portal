27f7bc0136d140fd7db4f4df9784db46f1059752
Tanvirjem <tanviranjumshozib@gmail.com>
2023-03-02 23:55:55 +0600

    fetch added

---
+++ news-portal.js
@@ -0,0 +1,28 @@
+const fetchCatagories = () => {
+    fetch('https://openapi.programming-hero.com/api/news/categories')
+        .then(res => res.json())
+        .then(data => showCategories(data.data))
+};
+
+const showCategories = data => {
+    console.log(data)
+    // capture categories container to append all the category links
+    const categoriesContainer = document.getElementById('categories-container');
+    data.news_category.forEach(singleCategory => {
+        // console.log(singleCategory);
+        // advanced - 1
+        /*         
+        categoriesContainer.innerHTML += 
+        `   
+        <a class="nav-link" href="#">${singleCategory?.category_name}</a>
+        `
+        */
+
+        // step: 2 (recommended for all us)
+        let linkContainer = document.createElement('p');
+        linkContainer.innerHTML = `
+        <a class="nav-link" href="#">${singleCategory?.category_name}</a>
+        `;
+        categoriesContainer.appendChild(linkContainer);
+    })
+}
